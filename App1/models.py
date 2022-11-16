96e12d65ad129e6b3166575c43d48a270e31095d
27_07 <27_07b@mail.ru>
2022-11-16 19:07:45 +0300

    __init__

---
+++ App1/models.py
@@ -0,0 +1,48 @@
+from django.db.models import ForeignKey, Model, CharField, IntegerField, TimeField, CASCADE, FloatField, SET_NULL
+
+
+class User(Model):
+    requisites = CharField(max_length=20)
+    number = CharField(max_length=10)
+    email = CharField(max_length=20)
+
+
+class Stadium(Model):
+    name = CharField(max_length=20)
+
+    street = CharField(max_length=20)
+    city = CharField(max_length=20)
+    region = CharField(max_length=20)
+    country = CharField(max_length=20)
+
+
+class Match(Model):
+    name = CharField(max_length=20)
+    stadium = ForeignKey(Stadium, on_delete=CASCADE)
+
+    side1 = CharField(max_length=20)
+    side2 = CharField(max_length=20)
+    coefficient = FloatField(default=1)
+
+    year = IntegerField(null=True)
+    day = IntegerField(null=True)
+    time = TimeField(null=True)
+
+
+class Sector(Model):
+    name = CharField(max_length=20)
+    price = IntegerField(null=True)
+
+
+class Place(Model):
+    sector = ForeignKey(Sector, on_delete=SET_NULL, null=True)
+    row = IntegerField()
+    count = IntegerField()
+    stadium = ForeignKey(Stadium, on_delete=CASCADE)
+
+
+class Ticket(Model):
+    host = ForeignKey(User, default=SET_NULL, on_delete=SET_NULL, null=True)
+    QR_code = CharField(max_length=1)
+    place = ForeignKey(Place, on_delete=CASCADE)
+    match = ForeignKey(Match, on_delete=CASCADE)
